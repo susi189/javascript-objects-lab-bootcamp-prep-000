812d33f901c1045a19ccda28d06bd3c52fb2406c
Susanna Brumm <susanna.brumm@gmail.com>
2018-02-16 01:34:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
-function Objects(obj) {
- var recipes = Object.assign({}, obj)
- return recipes
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var obj = Object.assign({}, object, {[key]: value})
+  return obj
 }
 
-//function updateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object
+}
+
+function deleteFromObjectByKey(object, key) {
   
 }
+
+
+  
